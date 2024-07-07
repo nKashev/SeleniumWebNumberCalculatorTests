2ffedb7a5e1ea92f4ecfacb9cb280b75f8cf7bd4
Nikolay Kashev <67652045+nKashev@users.noreply.github.com>
2024-07-07 16:04:41 +0300

    Update SeleniumWebNumberCalculatorTests.cs

--- Number Calculator/SeleniumWebNumberCalculatorTests.cs
+++ Number Calculator/SeleniumWebNumberCalculatorTests.cs
@@ -2,6 +2,8 @@ using System;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 
 namespace Number_Calculator
