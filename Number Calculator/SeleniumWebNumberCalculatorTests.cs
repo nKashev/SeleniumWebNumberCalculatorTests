c43ebd807a42d02f40bc963e537a8000e49565e1
Nikolay Kashev <67652045+nKashev@users.noreply.github.com>
2024-07-08 15:12:12 +0300

    Update SeleniumWebNumberCalculatorTests.cs

--- Number Calculator/SeleniumWebNumberCalculatorTests.cs
+++ Number Calculator/SeleniumWebNumberCalculatorTests.cs
@@ -70,31 +70,31 @@ namespace Number_Calculator
                     return new EdgeDriver(edgeOptions);
 
                 case "Firefox":
-                    // var geckoDriverPath = Environment.GetEnvironmentVariable("GECKODRIVER_PATH") ?? "/usr/local/bin/geckodriver";
-                    // var firefoxBinaryPath = Environment.GetEnvironmentVariable("FIREFOX_PATH") ?? "/usr/bin/firefox";
-
-                    // var firefoxOptions = new FirefoxOptions();
-                    // firefoxOptions.AddArgument("--headless");
-                    // firefoxOptions.BinaryLocation = firefoxBinaryPath;
-
-                    // var firefoxService = FirefoxDriverService.CreateDefaultService(geckoDriverPath);
-                    // firefoxService.Host = "::1"; // Use IPv6 loopback address
-
-                    // return new FirefoxDriver(firefoxService, firefoxOptions);
+                    var geckoDriverPath = GetDriverPath("geckodriver");
+                    var firefoxBinaryPath = GetDriverPath("firefox");
 
                     var firefoxOptions = new FirefoxOptions();
                     firefoxOptions.AddArgument("--headless");
+                    firefoxOptions.BinaryLocation = firefoxBinaryPath;
 
-                    // Specify the path to the Firefox binary if needed
-                    firefoxOptions.BinaryLocation = "/usr/bin/firefox";
-
-                    var firefoxService = FirefoxDriverService.CreateDefaultService();
+                    var firefoxService = FirefoxDriverService.CreateDefaultService(geckoDriverPath);
                     firefoxService.Host = "::1"; // Use IPv6 loopback address
 
                     // Increase command timeout
                     var commandTimeout = TimeSpan.FromMinutes(3);
+                    
+                    return new FirefoxDriver(firefoxService, firefoxOptions);
 
-                    return new FirefoxDriver(firefoxService, firefoxOptions, commandTimeout);
+                    // var firefoxOptions = new FirefoxOptions();
+                    // firefoxOptions.AddArgument("--headless");
+
+                    // // Specify the path to the Firefox binary if needed
+                    // firefoxOptions.BinaryLocation = "/usr/bin/firefox";
+
+                    // var firefoxService = FirefoxDriverService.CreateDefaultService();
+                    // firefoxService.Host = "::1"; // Use IPv6 loopback address
+
+                    // return new FirefoxDriver(firefoxService, firefoxOptions, commandTimeout);
 
                 case "Brave":
                     var braveDriverPath = GetDriverPath("brave");
